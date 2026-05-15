8bb8524b29fe9a558382b0b9cffb7b1d42e659be
Raghavnadiminti <raghavnadiminti@gmail.com>
2026-05-15 10:48:09 +0530

    bug fix

--- routes/githubapi.js
+++ routes/githubapi.js
@@ -152,7 +152,7 @@ try {
 gitApirouter.get("/projects", auth, async (req, res) => {
   try {
     const projects = await Project.find({ user: req.user._id })
-      .select("repoName repoFullName defaultBranch repoId createdAt");
+      .select("repoName repoFullName defaultBranch repoId createdAt status");
 
     res.json({
       count: projects.length,
